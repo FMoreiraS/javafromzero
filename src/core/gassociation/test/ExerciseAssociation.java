cb7b2844d831a595f5a47b918d3dccb98d26c045
FMoreiraS <mfellipeds@hotmail.com>
2025-05-22 10:15:07 -0300

    create all entities and a method to register; there are problems

--- src/core/gassociation/test/ExerciseAssociation.java
+++ src/core/gassociation/test/ExerciseAssociation.java
@@ -17,40 +17,176 @@ Local: endereço
  */
 
 public class ExerciseAssociation {
+    public static Seminary[] registeredSeminaries;
+    static int arrayIndex;
+
     public static void main(String[] args) {
+        Seminary metaphysicalView = new Seminary("Visão Metafísica da Realidade");
+        Professor profGregorio = new Professor("Gregório Santana", "Metafísica");
+        Academician carlos = new Academician("Carlos Braga", 22);
+        Local auditorium = new Local("Auditório da Universidade");
 
+        registerWithoutScanner(metaphysicalView, profGregorio, auditorium, carlos);
+        registeredSeminaries[0].print();
     }
+    // Evitei scanner para ganhar tempo.
+    public static void registerWithoutScanner(Seminary seminary, Professor teacher, Local address, Academician... participants) {
+//        Seminary metaphysicalView = new Seminary("Visão Metafísica da Realidade");
+//        Professor profGregorio = new Professor("Gregório Santana", "Metafísica");
+//        Academician carlos = new Academician("Carlos Braga", 22);
+//        Local auditorium = new Local("Auditório da Universidade");
 
+        teacher.setSeminaries(seminary);
+        address.setSeminary(seminary);
+        //carlos.setSeminary(metaphysicalView);
+
+        seminary.setProfessor(teacher);
+        seminary.setLocal(address);
+        seminary.setParticipants(participants);
+
+        incrementSeminaries(seminary);
+    }
+    public static void incrementSeminaries(Seminary seminary) {
+        if(arrayIndex == 0) {
+            registeredSeminaries[arrayIndex] = seminary;
+            arrayIndex++;
+            return;
+        }
+        registeredSeminaries[arrayIndex] = seminary;
+    }
+    // Por razões práticas, todas as entidades envolvidas estão aqui.
     public class Seminary {
         private String title;
         private Professor teacher;
         private Local address;
-        private Academic[] participants;
+        private Academician[] participants;
+
+        public Seminary(String title) {
+            this.title = title;
+        }
 
         public Seminary(String title, Professor teacher, Local address) {
             this.title = title;
             this.teacher = teacher;
             this.address = address;
+        }
 
-            public void setSeminaries(Academic... participants) {
-                this.participants = participants;
+        public void print() {
+            System.out.println("Seminário " + getTitle());
+            System.out.print("ministrado pelo professor " + this.teacher.getName());
+            System.out.println(", especialista em " + this.teacher.getSpecialty());
+            System.out.println("Local: " + this.address.getAddress());
+            System.out.println("Participantes:");
+            for(int i = 0; i < this.participants.length; i++) {
+                System.out.println((i+1) + this.participants[i].getName());
             }
+        }
+
+        public void setProfessor(Professor teacher) {
+            this.teacher = teacher;
+        }
+
+        public void setLocal(Local address) {
+            this.address = address;
+        }
+
+        public void setParticipants(Academician... participants) {
+            this.participants = participants;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Professor getTeacher() {
+            return teacher;
+        }
+
+        public Local getAddress() {
+            return address;
+        }
+
+        public Academician[] getParticipants() {
+            return participants;
         }
     }
 
     public class Professor {
         private String name;
-        private String especialty;
+        private String specialty;
         private Seminary[] seminaries;
 
-        public Professor(String name, String especialty) {
+        public Professor(String name, String specialty) {
             this.name = name;
-            this.especialty = especialty;
+            this.specialty = specialty;
+        }
+
+        public void print() {
+            System.out.println("Professor " + this.getName());
         }
 
         public void setSeminaries(Seminary... seminaries) {
             this.seminaries = seminaries;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSpecialty() {
+            return specialty;
+        }
+
+        public Seminary[] getSeminaries() {
+            return seminaries;
+        }
     }
-    // todo: criar classe Academic com seus campos. Pensar no funcionamento geral do cadastro.
+
+    public class Local {
+        private String address;
+        private Seminary seminary;
+
+        public Local(String address) {
+            this.address = address;
+        }
+
+        public void setSeminary(Seminary seminary) {
+            this.seminary = seminary;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public Seminary getSeminary() {
+            return seminary;
+        }
+    }
+
+    public class Academician {
+        private String name;
+        private int age;
+        private Seminary seminary;
+
+        public Academician(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Seminary getSeminary() {
+            return seminary;
+        }
+
+        public void setSeminary(Seminary seminary) {
+            this.seminary = seminary;
+        }
+    }
 }
