8a4e1622f7ca0fa84875b9341bf8a443189c8305
FMoreiraS <mfellipeds@hotmail.com>
2025-12-03 09:11:40 -0300

    test: update test for search by enum attributes

--- src/core/kenums/test/EnumTest.java
+++ src/core/kenums/test/EnumTest.java
@@ -13,22 +13,26 @@ public class EnumTest {
         System.out.println(customer1);
         System.out.println(customer2);
         
-        System.out.println("Teste do enum interno");
+        System.out.println("\nTeste do enum interno");
         Customer customer3 = new Customer("Cinthia", PaymentType.CREDIT);
         Customer customer4 = new Customer("Magda", PaymentType.DEBT);
         System.out.println(customer3);
         System.out.println(customer4);
 
-        System.out.println("Teste do atributo tipo String NAME");
+        System.out.println("\nTeste do atributo tipo String NAME");
         Customer customer5 = new Customer("Alexander", StatesOfBrazil.SAO_PAULO);
         System.out.println(customer5.getInfo());
 
-        System.out.println("Teste do valueOf()");
+        System.out.println("\nTeste do valueOf() com valor da enumeração");
         // Só consegue retornar um valor da enumeração se receber o
         // nome real, não um atributo, como o nome formatado.
         StatesOfBrazil stateSP = StatesOfBrazil.valueOf("SAO_PAULO");
         System.out.println(stateSP.FORMATTED_NAME);
-        System.out.println("Teste da busca por atributo");
+        System.out.println("\nTeste da busca por atributo");
         StatesOfBrazil statePA = StatesOfBrazil.getStateFromFormattedName("Pará");
+        System.out.println(
+            "Usando o atributo \"Pará\"(FORMATTED_NAME) conseguimos outro (ABBREVIATION): " +
+            statePA.ABBREVIATION
+        );
     }
 }
