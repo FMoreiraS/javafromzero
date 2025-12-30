8030b3a8e6919a52c67ac1415654d949491bd280
FMoreiraS <mfellipeds@hotmail.com>
2025-12-30 08:20:45 -0300

    test: update ComputerTest in core.npolimorphism

--- src/core/npolimorphism/test/ComputerTest.java
+++ src/core/npolimorphism/test/ComputerTest.java
@@ -9,15 +9,18 @@ public class ComputerTest {
         // TESTANDO POLIMORFISMO
         // 1. Widening cast
         Computer desktop = new Desktop("PC9710", "01/2029");
-        Computer laptop = new Desktop("LP8906", "02/2029");
-        // Usando o cast no laptop, é impossível acessar "openLid()".
-        // Resolvemos criando um objeto com o tipo específico
-        Laptop koreatop = new Laptop("KoreaTop30+", "01/2028");
-        koreatop.openLid();
-
-        // Usar Computer nas variáveis de referência permite isto:
+        Computer laptop = new Laptop("LP8906", "02/2029");
+        // Assim podemos acessar o método de Computer que recebe objetos
+        // Computer como parâmetro:
         Computer.showInfo(desktop);
+        // Num IDE, observe-se na depuração o funcionamento de "instanceof",
+        // colocando um breakpoint na linha 18.
         Computer.showInfo(laptop);
         
+        // Tendo usado o cast num laptop, é impossível acessar "openLid()".
+        // laptop.openLid();
+        // É compulsório criar um objeto com o tipo específico:
+        Laptop koreatop = new Laptop("KoreaTop30+", "01/2028");
+        koreatop.openLid();
     }
 }
